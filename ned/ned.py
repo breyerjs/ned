0cd70c1473a6062243d48460fb94367e2f298282
breyerjs <breyer.js@gmail.com>
2018-06-16 14:04:19 -0700

    Fix the reconnection issue

--- ned/ned.py
+++ ned/ned.py
@@ -88,18 +88,14 @@ def process_commands(commands):
     return processed
 
 if __name__ == "__main__":
-    if slack_client.rtm_connect(with_team_state=False):
+    if slack_client.rtm_connect(with_team_state=False, auto_reconnect=True):
         print("Ned is connected and running!")
         # Read bot's user ID by calling Web API method `auth.test`
         ned_id = slack_client.api_call("auth.test")["user_id"]
         while True:
-            try:
-                command, channel = parse_bot_commands(slack_client.rtm_read())
-                if command:
-                    handle_command(command, channel)
-                time.sleep(RTM_READ_DELAY)
-            except:
-                print("We hit one of those errors that usually makes ned crash")
-                time.sleep(20)
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
     else:
         print("Ruh roh! Connection failed. Exception traceback printed above.")
