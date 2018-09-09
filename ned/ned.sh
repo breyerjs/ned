d17c99a687f8841f89241955967a2d627a33fc40
jackson <breyer.js@gmail.com>
2018-09-09 06:17:22 +0000

    Fix script

--- ned/ned.sh
+++ ned/ned.sh
@@ -3,5 +3,5 @@ while :  # while true
 do
     # If there's no python scripts running, kick ned off
     # If there are, loop
-    pgrep -lf python; if [ $? != 0 ]; then python ned.py; fi
+    pgrep -lf python; if [ $? != 0 ]; then python3 ned.py; fi
 done
